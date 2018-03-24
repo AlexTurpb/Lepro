644d6810e329ebea000bf0f0a3d42517f136ec4d
Alex_Turpb <turpb41@gmail.com>
2018-03-24 15:26:57 +0200

    save content to db

--- app.rb
+++ app.rb
@@ -38,5 +38,7 @@ post '/new' do
 		@error = "Enter post text"
 		return erb :new
 	end
+
+	@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 	erb "You typed #{content}"
 end
